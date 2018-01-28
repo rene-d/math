5f964101cd8a56c7c7abae3631e16e6914b4c7f8
rene-d <rene@pc.home>
2018-01-28 02:10:35 +0100

    plus dans l'esprit Python

--- fractales/fractales.py
+++ fractales/fractales.py
@@ -2,15 +2,12 @@
 # rené 2018
 
 """ courbes fractales
-
 https://fr.wikipedia.org/wiki/Flocon_de_Koch
-
 """
 
-import sys
+import sys, time, math
 import tkinter as tk
 from typing import NamedTuple
-import math
 
 
 class point2D(NamedTuple):
@@ -23,19 +20,6 @@ class pointF(NamedTuple):
     y: float
     sens: bool
 
-class rect2D(NamedTuple):
-    x1: float
-    y1: float
-    x2: float
-    y2: float
-
-
-root = None
-repere = None
-courant = None
-canvas = None
-couleur = "red"
-
 
 def sqr(x):
     return x * x
@@ -45,205 +29,177 @@ def sqrt(x):
     return math.sqrt(x)
 
 
-def initFract(nom):
-    global gen
+class Fractale:
 
-    if nom == "Mandelbrot":
-        # «courbe de Mandelbrot» : exemple d'une courbe de Peano
-        gen = [
-            pointF(-1,                0, False),    #0
-            pointF(-2/3.,     sqrt(3)/3, True),     #1
-            pointF(-1/3.,   2*sqrt(3)/3, True),     #2
-            pointF( 1/3.,   2*sqrt(3)/3, True),     #3
-            pointF( 2/3.,     sqrt(3)/3, True),     #4
-            pointF( 1/3.,   4*sqrt(3)/9, False),    #5
-            pointF(    0,   5*sqrt(3)/9, False),    #6
-            pointF(-1/3.,   4*sqrt(3)/9, False),    #7
-            pointF(-1/3.,   2*sqrt(3)/9, True),     #8
-            pointF( 1/3.,   2*sqrt(3)/9, True),     #9
-            pointF(    0,     sqrt(3)/9, False),    #10
-            pointF(-1/3.,             0, False),    #11
-            pointF( 1/3.,             0, True),     #12
-            pointF(    1,             0, True),     #13
-        ]
+    def __init__(self, nom):
+        if nom == "Mandelbrot":
+            # «courbe de Mandelbrot» : exemple d'une courbe de Peano
+            self.gen = [
+                pointF(    -1,                 0, False),    # 0
+                pointF(-2 / 3,       sqrt(3) / 3, True),     # 1
+                pointF(-1 / 3,   2 * sqrt(3) / 3, True),     # 2
+                pointF( 1 / 3,   2 * sqrt(3) / 3, True),     # 3
+                pointF( 2 / 3,       sqrt(3) / 3, True),     # 4
+                pointF( 1 / 3,   4 * sqrt(3) / 9, False),    # 5
+                pointF(     0,   5 * sqrt(3) / 9, False),    # 6
+                pointF(-1 / 3,   4 * sqrt(3) / 9, False),    # 7
+                pointF(-1 / 3,   2 * sqrt(3) / 9, True),     # 8
+                pointF( 1 / 3,   2 * sqrt(3) / 9, True),     # 9
+                pointF(    0,        sqrt(3) / 9, False),    # 10
+                pointF(-1 / 3,                 0, False),    # 11
+                pointF( 1 / 3,                 0, True),     # 12
+                pointF(     1,                 0, True),     # 13
+            ]
 
-    elif nom == "Koch":
-        gen = [
-            pointF(-1,          0, True),    #0
-            pointF(-1/3.,       0, True),    #1
-            pointF(0,   sqrt(3)/3, True),    #2
-            pointF(1/3.,        0, True),    #3
-            pointF(1,           0, True),    #4
-        ]
+        elif nom == "Koch":
+            self.gen = [
+                pointF(-1,           0, True),    # 0
+                pointF(-1/3,         0, True),    # 1
+                pointF(0,  sqrt(3) / 3, True),    # 2
+                pointF(1/3,          0, True),    # 3
+                pointF(1,            0, True),    # 4
+            ]
 
-    elif nom == "Cesaro":
-        alpha = math.radians(85)        # angle de la pointe, 60° pour von Koch
-        a = 1 / (1 + math.cos(alpha))   # longueur de chaque segment _/\_
-        gen = [
-            pointF(    -1,                   0, True),    #0
-            pointF(-1 + a,                   0, True),    #1
-            pointF(     0, a * math.sin(alpha), True),    #2
-            pointF( 1 - a,                   0, True),    #3
-            pointF(     1,                   0, True),    #4
-        ]        
+        elif nom == "Cesaro":
+            alpha = math.radians(85)        # angle de la pointe, 60° pour von Koch
+            a = 1 / (1 + math.cos(alpha))   # longueur de chaque segment _/\_
+            self.gen = [
+                pointF(1, 0, True),                     # 0
+                pointF(-1 + a, 0, True),                # 1
+                pointF(0, a * math.sin(alpha), True),   # 2
+                pointF(1 - a, 0, True),                 # 3
+                pointF(1, 0, True),                     # 4
+            ]
 
-    elif nom == "Peano":
-        gen = [
-            pointF(-3, 0, True),
-            pointF(-1, 0, True),
-            pointF(-1, 2, True),
-            pointF(1, 2, True),
-            pointF(1, 0, True),    
-            pointF(1, -2, True),
-            pointF(-1, -2, True),
-            pointF(-1, 0, True),
-            pointF(1, 0, True),
-            pointF(3, 0, True),
-        ]
-
-premier = True
+        elif nom == "Peano":
+            self.gen = [
+                pointF(-3, 0, True),
+                pointF(-1, 0, True),
+                pointF(-1, 2, True),
+                pointF(1, 2, True),
+                pointF(1, 0, True),
+                pointF(1, -2, True),
+                pointF(-1, -2, True),
+                pointF(-1, 0, True),
+                pointF(1, 0, True),
+                pointF(3, 0, True),
+            ]
 
 
-def conv(p):
-    x,y = (p.x - repere.x1) * repere.x2, (p.y - repere.y1) * repere.y2
-    return x,y
+class Crt:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width=1024, height=768,
+                                borderwidth=0, highlightthickness=0)
+        self.canvas.grid()
+
+    def repere(self, x_min, x_max, y_min, y_max):
+        bounds = [0, 0, 1024, 768]
+        self.coords = [x_min,
+                       y_max,
+                       (bounds[2] - bounds[0]) / (x_max - x_min),
+                       (bounds[3] - bounds[1]) / (y_min - y_max)]
+
+    def conv(self, p):
+        return (p.x - self.coords[0]) * self.coords[2], (p.y - self.coords[1]) * self.coords[3]
+
+    def ligne(self, p1, p2, fill="red"):
+        x1, y1 = self.conv(p1)
+        x2, y2 = self.conv(p2)
+        self.canvas.create_line(x1, y1, x2, y2, fill=fill)       # dash=(2,16)
+
+    def clear(self):
+        self.canvas.delete("all")
+
+    def bind_key(self, callback):
+        self.canvas.bind("<Key>", callback)
 
 
-def initgraphique():
-    global canvas, repere, courant, root
-    root = tk.Tk()
-    canvas = tk.Canvas(root, width=1024, height=768, borderwidth=0, highlightthickness=0)
-    canvas.grid()
-    
+class Dessine:
+    def __init__(self, nom):
+        self.crt = Crt()
+        self.fractale = Fractale(nom)
+        self.generation = 0
+        self.nom = nom
 
-def fenetre(x1, x2, y1, y2):
-    global canvas, repere, courant, root
+        ratio = 1024 / 768 * 8.
+        self.crt.repere(-ratio / 2, ratio / 2, -2, 6)
 
-    bounds = [0,0,1024,768]
-    repere = rect2D(
-        x1, 
-        y2,
-        (bounds[2] - bounds[0]) / (x2 - x1),
-        (bounds[3] - bounds[1]) / (y1 - y2) 
-    )
+        self.dessinefractale(point2D(-2, 0), point2D(2, 0), True, 0, "black")
 
-def deplace(p):
-    global canvas, repere, courant
-    courant = p
+        self.crt.canvas.focus_set()
+        self.dessine()
 
-def trace(p, fill="red"):
-    global canvas, repere, courant
-    x1, y1 = conv(courant)
-    x2, y2 = conv(p)
-    #canvas.create_oval(x1-2,y1-2,x1+2,y1+2)
-    canvas.create_line(x1, y1, x2, y2, fill=fill)
-    courant = p
+        self.crt.root.mainloop()
 
-def trait(x1, y1, x2,y2):
-    xx1, yy1 = conv(point2D(x1, y1))
-    xx2, yy2 = conv(point2D(x2, y2))
-    canvas.create_line(xx1, yy1, xx2, yy2, fill="black", dash=(2, 16))
+    def dessinefractale(self, ori, ext, sensf, ordre, fill="red"):
+        # calcul coefficients de la transformation : [seg1,seg2] --> [ori,ext]
 
-def zz():
-    root = tk.Tk()
-    canvas = tk.Canvas(root, width=1024, height=768, borderwidth=0, highlightthickness=0)
-    canvas.grid()
-    canvas.create_oval(0,0,50,30)
-    canvas.create_line(0,0,300,300, fill="red")
-    canvas.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-    canvas.create_rectangle(50, 25, 150, 75, fill="blue")
-    print(canvas.canvasx(400))
-    print(canvas.canvasy(400))
+        # sensf=true  => similitude directe
+        # sensf=false => similitude indirecte
+        anti = 1 if sensf else -1
 
-    root.wm_title("Circles and Arcs")
-    root.mainloop()
+        seg1 = self.fractale.gen[0]
+        seg2 = self.fractale.gen[-1]
 
+        dsx = seg1.x - seg2.x
+        dsy = seg1.y - seg2.y
+        m = sqr(dsx) + sqr(dsy)
+        dx = ori.x - ext.x
 
-def dessinefractale(ori, ext, sensf, ordre, fill="red"):
-    global sens, gen, premier
+        dy = ori.y - ext.y
+        C = (dsx * dx + dsy * dy * anti) / m
+        S = (dsx * dy - dsy * dx * anti) / m
 
-    # calcul coefficients de la transformation : [seg1,seg2] --> [ori,ext] 
+        A = ori.x - (C * seg1.x - S * seg1.y * anti)
+        B = ori.y - (S * seg1.x + C * seg1.y * anti)
 
-    # sensf=true  => similitude directe
-    # sensf=false => similitude indirecte
-    anti = 1 if sensf else -1
+        # on a : x' = C*x-S*y*anti+A  et  y' = S*x+C*y*anti+B
 
-    seg1 = gen[0]
-    seg2 = gen[len(gen) - 1]
-
-    dsx = seg1.x - seg2.x
-    dsy =seg1.y-seg2.y
-    m = sqr(dsx)+sqr(dsy)
-    dx =ori.x-ext.x
-
-    dy=ori.y-ext.y
-    C=(dsx*dx+dsy*dy*anti)/m
-    S=(dsx*dy-dsy*dx*anti)/m
-
-    A=ori.x-(C*seg1.x-S*seg1.y*anti)
-    B=ori.y-(S*seg1.x+C*seg1.y*anti)
-
-    # { on a : x' = C*x-S*y*anti+A  et  y' = S*x+C*y*anti+B }
-
-    if ordre <= 0:
-        for i in gen:
-            p = point2D(C*i.x-S*i.y*anti+A, S*i.x+C* i.y*anti+B)   
-            if premier: 
-                deplace(p)
-                premier = False
-            else:
-                trace(p, fill)
-    else:
         ori = None
         sens_ori = None
-        for i in gen:
-            p = point2D(C*i.x-S*i.y*anti+A, S*i.x+C*i.y*anti+B)    
+        for i in self.fractale.gen:
+            p = point2D(C * i.x - S * i.y * anti + A, S * i.x + C * i.y * anti + B)
             if ori is not None:
                 ext = p
-                dessinefractale(ori, ext, not sens_ori ^ sensf, ordre - 1, fill)
+
+                if ordre == 0:
+                    self.crt.ligne(ori, ext, fill)
+                else:
+                    self.dessinefractale(ori, ext, not sens_ori ^ sensf, ordre - 1, fill)
             ori = p
             sens_ori = i.sens
 
+    def dessine(self):
+        self.crt.bind_key(None)
+        self.crt.clear()
+        self.crt.root.wm_title("Fractales : {} génération {} [en cours...]".format(self.nom, self.generation))
+        start_time = time.time()
 
-nn = 0
+        couleur = "black" if self.generation == 0 else "red"
+        self.dessinefractale(point2D(-2, 0), point2D(2, 0), True, self.generation, fill=couleur)
+        self.crt.bind_key(self.callback)
 
-def callback(event):
-    global canvas, repere, courant, root, nn, premier
-    #print("callback:", event)
-    if event.char == '+' and nn < 5:
-        nn += 1
-        canvas.delete("all")
-        premier = True
-        dessinefractale(point2D(-2, 0), point2D(2, 0), True, nn)
-    elif event.char == '-' and nn > 0:
-        nn -= 1
-        canvas.delete("all")
-        premier = True
-        dessinefractale(point2D(-2, 0), point2D(2, 0), True, nn)
+        self.crt.root.wm_title("Fractales : {} génération {} [{:.3f} secondes]".format(self.nom, self.generation, time.time() - start_time))
 
+    def callback(self, event):
+        # print("callback:", event)
+        old_generation = self.generation
+        if event.char == '+' and self.generation < 5:
+            self.generation += 1
+        elif event.char == '-' and self.generation > 0:
+            self.generation -= 1
 
-def dessine(nom):    
-    global canvas, repere, courant
-    initgraphique()
-
-    initFract(nom)
-
-    ratio = 1024 / 768 * 8.
-    fenetre(-ratio/2, ratio/2, -2, 6)
-
-    dessinefractale(point2D(-2,0), point2D(2,0), True, 0, "black")
-
-    canvas.bind("<Key>", callback)
-    canvas.focus_set()
-
-    root.mainloop()
+        if self.generation != old_generation:
+            self.dessine()
 
 
 def main():
     if len(sys.argv) >= 2:
-        dessine(sys.argv[1])
+        Dessine(sys.argv[1])
     else:
-        dessine("Koch")
+        Dessine("Koch")
+
 
 if __name__ == '__main__':
     main()
