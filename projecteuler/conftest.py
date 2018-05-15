283ce6c7b0a2dd90890c33e309fb0b1f95b4e2b9
Rene D <rene.devichi@gmail.com>
2018-05-15 13:49:54 +0200

    Update conftest.py

--- projecteuler/conftest.py
+++ projecteuler/conftest.py
@@ -7,7 +7,7 @@ def pytest_sessionstart(session):
     pe_dir = os.path.dirname(__file__)
     os.chdir(pe_dir)
     os.makedirs("build", exist_ok=True)
-    res = subprocess.run("cd build && cmake .. && make V=1", shell=True, stderr=None)
+    res = subprocess.run("cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make V=1", shell=True, stderr=None)
     if res.returncode != 0:
         # pytest.fail("C/C++ build failed")
         print("C/C++ build failed - will skip native tests")
