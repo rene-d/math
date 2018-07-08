3863301f5eaf4497bfd072db30108e028f23b564
René Devichi <rene.devichi@gmail.com>
2018-07-08 23:24:13 +0200

    erreur de range

--- projecteuler/p011.py
+++ projecteuler/p011.py
@@ -37,25 +37,25 @@ for i in grid_def.split():
 pmax = 0
 
 for i in range(20):
-    for j in range(16):
+    for j in range(17):
         p = grid[i][j] * grid[i][j + 1] * grid[i][j + 2] * grid[i][j + 3]
         if p > pmax:
             pmax = p
 
-for i in range(16):
+for i in range(17):
     for j in range(20):
         p = grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j]
         if p > pmax:
             pmax = p
 
-for i in range(16):
-    for j in range(16):
+for i in range(17):
+    for j in range(17):
         p = grid[i][j] * grid[i + 1][j + 1] * grid[i + 2][j + 2] * grid[i + 3][j + 3]
         if p > pmax:
             pmax = p
 
 for i in range(3, 20):
-    for j in range(16):
+    for j in range(17):
         p = grid[i][j] * grid[i - 1][j + 1] * grid[i - 2][j + 2] * grid[i - 3][j + 3]
         if p > pmax:
             pmax = p
